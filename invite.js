0bb8b4cfce0af1e633af193d4ab8b978e8e93522
Abdalla Beniamen <abdallabeniamen622@gmail.com>
2026-09-17 11:33:08 +0300

    Update invite.js

--- invite.js
+++ invite.js
@@ -20,37 +20,5 @@
     }
     updateCountdown();
     setInterval(updateCountdown, 1000);
-    // ==============================================
 
-// function createHeart() { 
-//   const heart = document.createElement('div');
-//   heart.classList.add('floating-heart');
-//   heart.innerHTML = '❤'; 
 
-//   const randomLeft = Math.random() * 90 + 5;
-//   heart.style.left = `${randomLeft}vw`;
-
-//   const randomSize = Math.floor(Math.random() * 16) + 16; 
-//   heart.style.fontSize = `${randomSize}px`;
-
-//   const randomDuration = (Math.random() * 1 + 2).toFixed(1);
-//   heart.style.animationDuration = `${randomDuration}s`;
-
-//   document.body.appendChild(heart);
-
-//   setTimeout(() => {
-//     heart.remove();
-//   }, randomDuration * 1000);
-// }
-
-// let isScrolling = false;
-// window.addEventListener('scroll', () => {
-
-//   if (!isScrolling) {
-//     isScrolling = true;
-//     createHeart();
-//     setTimeout(() => {
-//       isScrolling = false;
-//     }, 120); 
-//   }
-// });
